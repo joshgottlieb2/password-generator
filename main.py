369dc46650b4883cafbd07b3af6af5468daf9be7
joshgottlieb2 <joshgottlieb2@gmail.com>
2022-04-22 10:35:54 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# Select number of letters, symbols, and numbers in password
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+# Create empty password list
+password_list = []
+
+# Append randomly chosen letters, symbols, and numbers to password list
+for char in range(nr_letters):
+  password_list.append(random.choice(letters))
+
+for char in range(nr_symbols):
+  password_list += random.choice(symbols)
+
+for char in range(nr_numbers):
+  password_list += random.choice(numbers)
+
+# Randomly shuffle password characters
+random.shuffle(password_list)
+
+# Loop through password list to add characters to password string
+password = ""
+for char in password_list:
+  password += char
+
+# Print password to console
+print(f"Your password is: {password}")
